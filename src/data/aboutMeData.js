7549f6b2194f9e50aa7cf24f47c9a59011f534ec
decagon <chiorlujack@gmail.com>
2023-08-18 09:01:31 +0100

    initial commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,13 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: `Absolutely, Jack Chiorlu! With a diverse skill set encompassing Java, PHP, React, Vue, CSS, styled components, Spring Boot, JavaScript, Node.js, Express, Laravel, CodeIgniter, and Flutter, I am a dynamic and versatile developer. My journey in the world of programming has been fueled by a passion for crafting exceptional digital experiences.
+
+		Having worked with an array of cutting-edge technologies, I possess a holistic view of web and application development. My projects span from building robust backend systems to creating captivating user interfaces that enhance engagement and usability. My dedication to staying current with industry trends keeps me at the forefront of innovative solutions.
+		
+		My pursuit of excellence extends beyond technical proficiency; I am an advocate for clean, efficient code and have a knack for problem-solving and collaboration. My mission is to translate ideas into functional, user-centered realities while fostering a collaborative and inclusive work environment.
+		
+		I thrive on challenges and am excited by the ever-evolving landscape of technology. Each project is an opportunity to blend creativity with technical prowess, and I am enthusiastic about continuously pushing boundaries to shape remarkable digital landscapes.`,
+	},
+	
+];
---
+++ src/data/projects.js
@@ -0,0 +1,71 @@
+// Import images
+import WebImage1 from '../images/web-project-1.jpg';
+import WebImage2 from '../images/web-project-2.jpg';
+import MobileImage1 from '../images/mobile-project-1.jpg';
+import MobileImage2 from '../images/mobile-project-2.jpg';
+import UIImage1 from '../images/ui-project-1.jpg';
+import UIImage2 from '../images/ui-project-2.jpg';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Voucher Asset',
+		category: 'Web Application',
+		link:"https://voucherasset.ng",
+		img: WebImage2,
+		ProjectHeader: {
+			title: 'Project Management UI - From Context',
+			publishDate: 'Jul 26, 2021',
+			tags: 'Backned / Frontend',
+		},
+	},
+	{
+		id: 2,
+		title: 'Card Tonic Flutter',
+		category: 'Mobile Application',
+		link:"https://github.com/jwsvengit7/card-tonic-flutter-app",
+		img: MobileImage2,
+	},
+	{
+		id: 3,
+		title: 'Card Tonic Spring Boot',
+		category: 'Backend',
+		link:"https://github.com/jwsvengit7/cardtonic",
+		img: UIImage1,
+	},
+	{
+		id: 4,
+		title: 'Event Hub Spring Boot',
+		category: 'Backend',
+		link:"https://github.com/jwsvengit7/Hub-Event",
+		img: UIImage2,
+	},
+	{
+		id: 5,
+		title: 'React Music Library',
+		category: 'Web Application',
+		link:"https://github.com/jwsvengit7/groove",
+		img: MobileImage1,
+	},
+	{
+		id: 6,
+		title: 'Easy Lend Frontend',
+		category: 'Web Application',
+		link:"https://github.com/jwsvengit7/loan-app-FE",
+		img: WebImage1,
+	},
+	{
+		id: 7,
+		title: 'Easy Lend Spring Boot SOA',
+		category: 'Backend',
+		link:"https://github.com/jwsvengit7/easy-lend",
+		img: WebImage1,
+	},
+	{
+		id: 8,
+		title: 'Option IQ FX',
+		category: 'Web Application',
+		link:"https://www.options-iqfx.com/",
+		img: WebImage1,
+	},
+];
